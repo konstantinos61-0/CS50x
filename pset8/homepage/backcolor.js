0a9f68a04ed0f0e17b6c4296607eaa35316931ff
konstantinos61-0 <konstantinos.exad@gmail.com>
2026-03-16 03:22:48 +0200

    Add files via upload

---
+++ pset8/homepage/backcolor.js
@@ -0,0 +1,20 @@
+function changeBackgroundColortoWhite()
+{
+    let color = document.querySelector('body').style.backgroundColor = 'white';
+}
+
+function changeBackgroundColortoPurple()
+{
+    let color = document.querySelector('body').style.backgroundColor = 'purple'
+
+}
+
+function changeBackgroundColortoGreen()
+{
+    let color = document.querySelector('body').style.backgroundColor = 'green'
+}
+
+function changeBackgroundColortoOriginal()
+{
+    let color = document.querySelector('body').style.backgroundColor = '#99ccff'
+}
